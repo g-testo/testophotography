aca542ddaa31733054457fc54752f52d34e7ddb0
Greg <cryptomonger@hotmail.com>
2016-09-11 01:13:36 +0000

    installed Mongo

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/view', function(req, res, next) {
+  res.send('user view');
+});
+
 module.exports = router;
