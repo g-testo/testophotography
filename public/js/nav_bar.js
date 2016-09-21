47974e21c861319e40184deac872c941eaa93eda
Greg <cryptomonger@hotmail.com>
2016-09-21 00:52:10 +0000

    added navigation to artwork pages

--- public/js/nav_bar.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var nav = document.getElementById("nav_items");
-    if (nav.className === "nav_menu_items") {
-        nav.className += " responsive";
-    } else {
-        nav.className = "nav_menu_items";
-    }
-}
--- public/js/page_routes.js
+++ public/js/page_routes.js
@@ -5,33 +5,39 @@ app.config(function ($routeProvider) {
     
     .when('/', {
         templateUrl:"pages/app.html",
-        controller:"appCtrl"
         })
     .when('/artwork', {
         templateUrl:"pages/artwork_index.html",
-        controller:"artworkCtrl"
     })
     .when('/1x4', {
-        templateUrl:"pages/artwork_1x4.html",
-        controller:"artworkCtrl"
+        templateUrl:"pages/artwork/artwork_1x4.html",
     })
     .when('/2x2', {
-        templateUrl:"pages/artwork_2x2.html",
-        controller:"artworkCtrl"
+        templateUrl:"pages/artwork/artwork_2x2.html",
     })
     .when('/2x4', {
-        templateUrl:"pages/artwork_2x4.html",
-        controller:"artworkCtrl"
+        templateUrl:"pages/artwork/artwork_2x4.html",
     })
     .when('/custom', {
-        templateUrl:"pages/artwork_custom.html",
-        controller:"artworkCtrl"
+        templateUrl:"pages/artwork/artwork_custom.html",
     })
     .when('/about', {
         templateUrl:"pages/about_us.html",
-        controller:"artworkCtrl"
     })
     .otherwise({
 		redirectTo: '#/'
-      });
+    });
 });
+
+app.controller('sideNavController', sideNavController);
+    
+    function sideNavController ($scope, $mdSidenav) {
+        $scope.openLeftMenu = function() {
+            $mdSidenav('left').toggle();
+     };
+        $scope.closeSideNavPanel = function() {
+            $mdSidenav('left').close();
+    };
+}
+
+
