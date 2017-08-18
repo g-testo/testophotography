66cad6636c2f6be2f9a0175674b401e413eda5f2
Gregory Testo <cryptomonger@hotmail.com>
2017-08-18 13:00:25 -0700

    deployment

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 var express = require('express');
 var sass = require('node-sass');
 var sassMiddleware = require('node-sass-middleware');
